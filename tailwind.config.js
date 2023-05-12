7666e132f2ee647dcf460ac3d6ee7fb1e41f3ab1
sangeeta47 <sangeeta.panwar@altudo.co>
2023-05-12 19:44:15 +0530

    Added New Features in Tailwind Poc

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,15 @@ module.exports = {
           from: { transform: 'translate(0, 0)' },
           to: { transform: 'translate(calc(-100% + (6 * 300px)))' },
         },
+        wave: {
+          to: {
+            "margin-left": "-51%"
+          }
+        }
       },
       animation: {
         'carousel': 'carouselAnim 10s infinite alternate linear',
+         wave: "wave 1.5s ease-in-out infinite"
       },
     },
     fontFamily:{
